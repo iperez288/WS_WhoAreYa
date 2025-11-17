606f0c64c148f4d44639f1001f577375b8ea9cf2
ikergarciaprieto <ikergarciaprieto@gmail.com>
2025-11-17 18:19:55 +0100

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -21,3 +21,54 @@ let initState = function(what, solutionId) {
     return [state, addGuess]
     
 }
+function successRate (e){
+    return e.successRate;
+}
+
+let getStats = function(what) {
+    const existitu = localStorage.getItem(what);
+    if(existitu){
+        return JSON.parse(existitu);
+    }
+    else{
+        const hasStats =
+        {
+        winDistributions: [0,0,0,0,0,0,0,0],
+        gamesFailed: 0,
+        currentStreak: 0,
+        bestStreak: 0,
+        totalGames: 0,
+        successRate: 0,
+        };
+        
+        localStorage.setItem(what, JSON.stringify(hasStats))
+        return hasStats;
+    }
+
+    
+};
+
+
+function updateStats(t){
+    gamestats.totalGames++;
+    if (t < 8){
+        gamestats.winDistributions[t - 1]++;
+        gamestats.currentStreak++;
+        if (gamestats.currentStreak > gamestats.bestStreak) {
+            gamestats.bestStreak = gamestats.currentStreak;
+        }
+    }
+    else{
+        gamestats.currentStreak = 0;
+        gamestats.gamesFailed++;
+    }
+    totalWins = gamestats.winDistributions.reduce((acc, win) => acc + win, 0);
+    gamestats.successRate = Math.round((totalWins / gamestats.totalGames) * 100);
+
+    localStorage.setItem(gamestats, JSON.stringify(gamestats));
+
+    return gamestats;
+};
+
+
+let gamestats = getStats('gameStats');
