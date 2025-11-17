11507d4163a9cec5149bb6118d38ba91624a9669
ikergarciaprieto <ikergarciaprieto@gmail.com>
2025-11-17 18:56:08 +0100

    Update fragments.js

--- js/fragments.js
+++ js/fragments.js
@@ -49,13 +49,13 @@ let headless = function (inner) {
 }
 
 const stats = function () {
-    const {totalGames, bestStreak, currentStreak, successRate, gamesFailed, winDistribution} = getStats("gameStats");
+    const {totalGames, bestStreak, currentStreak, successRate, gamesFailed, winDistributions} = getStats("gameStats");
 
     let blocks = `<div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden sh
 adow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 dark:bg-gray-800"><div class="absolute right-4 top-4" id="closedialog"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="h-6 w-6 cursor-pointer dark:stroke-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg></div><div><div class="text-center"><h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100" id="headlessui-dialog-title-7">Statistics</h3><div class="mt-2"><div class="flex justify-center my-2"><div class="items-center justify-center m-1 w-1/4 dark:text-white"><div class="text-3xl font-bold">${totalGames}</div><div class="text-xs">Total tries</div></div><div class="items-center justify-center m-1 w-1/4 dark:text-white"><div class="text-3xl font-bold">${successRate}%</div><div class="text-xs">Success rate</div></div><div class="items-center justify-center m-1 w-1/4 dark:text-white"><div class="text-3xl font-bold">${currentStreak}</div><div class="text-xs">Current streak</div></div><div class="items-center justify-center m-1 w-1/4 dark:text-white"><div class="text-3xl font-bold">${bestStreak}</div><div class="text-xs">Best streak</div></div></div>
 <h4 class="cursor-pointer text-lg leading-6 font-medium text-gray-900 dark:text-gray-100" id="showHide">Show Guess Distribution</h4><div class="columns-1 justify-left m-2 text-sm dark:text-white">`
 
-    let sum = winDistribution.reduce(function (previousValue, currentValue) {
+    let sum = winDistributions.reduce(function (previousValue, currentValue) {
         return previousValue + currentValue;
     });
 
@@ -64,8 +64,8 @@ adow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full s
         blocks += `<div class="flex justify-left m-1">
                         <div class="items-center justify-center w-2">${i}</div>
                         <div class="rounded-full w-full ml-2">
-                            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-l-full" style="width: ${Math.ceil(winDistribution[i] / sum * 100) }%;">
-                                ${winDistribution[i]}
+                            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-l-full" style="width: ${Math.ceil(winDistributions[i] / sum * 100) }%;">
+                                ${winDistributions[i]}
                             </div>
                         </div>
                     </div>`
