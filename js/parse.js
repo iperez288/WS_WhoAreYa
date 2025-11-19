b5d863e91f880dd1c570cceaf7a831d62b9dc532
JOVANNIZ10 <ipriala37@gmail.com>
2025-11-19 18:57:07 +0100

    feat: player browse system improved

---
+++ js/parse.js
@@ -0,0 +1,8 @@
+export function parse(name, matchResult) {
+    if (!matchResult) return [{ text: name, highlight: false }];
+    return [
+        { text: name.slice(0, matchResult.start), highlight: false },
+        { text: name.slice(matchResult.start, matchResult.end), highlight: true },
+        { text: name.slice(matchResult.end), highlight: false }
+    ];
+}
